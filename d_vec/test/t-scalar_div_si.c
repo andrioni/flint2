5fbff3ecd6be25298c0e53103c6be9dc585a60b5
Alessandro Andrioni <alessandroandrioni@gmail.com>
2013-07-01 02:14:05 -0300

    Scalar products and approximations

---
+++ d_vec/test/t-scalar_div_si.c
@@ -0,0 +1,114 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2013 Alessandro Andrioni
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "flint.h"
+#include "d_vec.h"
+#include "ulong_extras.h"
+#include "long_extras.h"
+
+int
+main(void)
+{
+    int i, result;
+    flint_rand_t state;
+
+    printf("scalar_div_si....");
+    fflush(stdout);
+
+    flint_randinit(state);
+
+    /* Check aliasing of a and b */
+    for (i = 0; i < 1000 * flint_test_multiplier(); i++)
+    {
+        double *a, *b;
+        slong len = n_randint(state, 100);
+        slong n = z_randtest(state);
+
+        /* Check if the divisor is non-null */
+        if (n == 0)
+            n++;
+
+        a = _d_vec_init(len);
+        b = _d_vec_init(len);
+        _d_vec_randtest(a, state, len);
+
+        _d_vec_scalar_div_si(b, a, len, n);
+        _d_vec_scalar_div_si(a, a, len, n);
+
+        result = (_d_vec_equal(a, b, len));
+        if (!result)
+        {
+            printf("FAIL:\n");
+            _d_vec_print(a, len), printf("\n\n");
+            _d_vec_print(b, len), printf("\n\n");
+            abort();
+        }
+
+        _d_vec_clear(a, len);
+        _d_vec_clear(b, len);
+    }
+
+    /* Check agreement with _d */
+    for (i = 0; i < 1000 * flint_test_multiplier(); i++)
+    {
+        double *a, *b;
+        slong len, n;
+        double x;
+
+        len = n_randint(state, 100);
+        a = _d_vec_init(len);
+        b = _d_vec_init(len);
+        _d_vec_randtest(a, state, len);
+        n = z_randtest(state);
+
+        /* Check if the divisor is non-null */
+        if (n == 0)
+            n++;
+
+        x = n;
+
+        _d_vec_scalar_div_d(b, a, len, x);
+        _d_vec_scalar_div_si(a, a, len, n);
+
+        result = (_d_vec_equal(a, b, len));
+        if (!result)
+        {
+            printf("FAIL:\n");
+            _d_vec_print(a, len), printf("\n\n");
+            _d_vec_print(b, len), printf("\n\n");
+            printf("%li\n\n", n);
+            abort();
+        }
+
+        _d_vec_clear(a, len);
+        _d_vec_clear(b, len);
+    }
+
+    flint_randclear(state);
+    printf("PASS\n");
+    return 0;
+}
---
+++ d_vec/test/t-scalar_mul_d.c
@@ -0,0 +1,75 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2013 Alessandro Andrioni
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "flint.h"
+#include "d_vec.h"
+#include "double_extras.h"
+#include "ulong_extras.h"
+#include "long_extras.h"
+
+int
+main(void)
+{
+    int i, result;
+    flint_rand_t state;
+
+    printf("scalar_mul_d....");
+    fflush(stdout);
+
+    flint_randinit(state);
+
+    /* Check aliasing of a and b */
+    for (i = 0; i < 1000 * flint_test_multiplier(); i++)
+    {
+        double *a, *b;
+        slong len = n_randint(state, 100);
+        double n = d_randtest(state);
+
+        a = _d_vec_init(len);
+        b = _d_vec_init(len);
+        _d_vec_randtest(a, state, len);
+
+        _d_vec_scalar_mul_d(b, a, len, n);
+        _d_vec_scalar_mul_d(a, a, len, n);
+
+        result = (_d_vec_equal(a, b, len));
+        if (!result)
+        {
+            printf("FAIL:\n");
+            _d_vec_print(a, len), printf("\n\n");
+            _d_vec_print(b, len), printf("\n\n");
+            abort();
+        }
+
+        _d_vec_clear(a, len);
+        _d_vec_clear(b, len);
+    }
+
+    flint_randclear(state);
+    printf("PASS\n");
+    return 0;
+}
